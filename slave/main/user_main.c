b0ee6013ccc30763fa2a338c98ae56d12c55df21
Sirius <sirius@x250>
2019-07-10 23:27:07 +0800

    Try deep sleep

--- slave/main/user_main.c
+++ slave/main/user_main.c
@@ -14,14 +14,16 @@
 
 #include <stdio.h>
 
-/* #include "freertos/FreeRTOS.h" */
-/* #include "freertos/timers.h" */
+#include "freertos/FreeRTOS.h"
+#include "freertos/timers.h"
 
 /* #include "esp_log.h" */
 #include "esp_now.h"
+#include "esp_sleep.h"
 #include "esp_system.h"
 /* #include "esp_wifi.h" */
 
+#include "driver/gpio.h"
 #include "driver/pwm.h"
 
 #include "rcp.h"
@@ -37,6 +39,11 @@
 #define PWM_STALL_DUTY 2000
 #define PWM_ON_DUTY 2400
 #define PWM_OFF_DUTY 1600
+
+#define GPIO_OUTPUT_IO_LED 2
+#define GPIO_OUTPUT_PIN_SEL ((1 << GPIO_OUTPUT_IO_LED))
+
+void system_deep_sleep_instant(uint32_t us);
 
 static const char *TAG = "remote_switch_slave";
 static uint8_t peer_mac[ESP_NOW_ETH_ALEN] = {0xa0, 0x20, 0xa6, 0xb, 0x4d, 0x10};
@@ -62,12 +69,51 @@ static void rcp_cmd_recv_cb(const rcp_cmd_t *cmd) {
   ESP_ERROR_CHECK(pwm_start());
 }
 
+void gpio_init(void) {
+  gpio_config_t io_conf;
+  // disable interrupt
+  io_conf.intr_type = GPIO_INTR_DISABLE;
+  // set as output mode
+  io_conf.mode = GPIO_MODE_OUTPUT;
+  // bit mask of the pins that you want to set,e.g.GPIO15/16
+  io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL;
+  // disable pull-down mode
+  io_conf.pull_down_en = 0;
+  // disable pull-up mode
+  io_conf.pull_up_en = 0;
+  // configure GPIO with the given settings
+  ESP_ERROR_CHECK(gpio_config(&io_conf));
+}
+
+void sleepTask(void *p) {
+  vTaskDelay(3000 / portTICK_RATE_MS);
+
+  printf("going to sleep\n");
+  vTaskDelay(1000 / portTICK_RATE_MS);
+
+  esp_deep_sleep(3000000);
+  vTaskDelay(1000 / portTICK_RATE_MS);
+}
+
 void app_main(void) {
   printf("SDK version: %s\n", esp_get_idf_version());
+
+  gpio_init();
+  printf("gpio inited\n");
 
   ESP_ERROR_CHECK(pwm_init(PWM_PERIOD, duties, PWM_CHANNEL_NUM, pin_num));
   ESP_ERROR_CHECK(pwm_set_phases(phase));
   printf("pwm inited\n");
 
+  // doc says put this before esp_wifi_init and esp_deep_sleep
+  // 2 means "Radio calibration will not be done after the deep-sleep wakeup"
+  // https://docs.espressif.com/projects/esp8266-rtos-sdk/en/latest/api-reference/system/sleep_modes.html
+  esp_deep_sleep_set_rf_option(2);
+
   rcp_slave_init(peer_mac, rcp_cmd_recv_cb);
+  printf("rcp inited\n");
+
+  ESP_ERROR_CHECK(gpio_set_level(GPIO_OUTPUT_IO_LED, 0));
+
+  xTaskCreate(sleepTask, "sleepTask", 1024, NULL, 2, NULL);
 }
